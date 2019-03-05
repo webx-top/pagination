30702e5f73f125230f63b1af02b1df8500fdbf6b
Hank Shen <swhbox@foxmail.com>
2019-03-05 21:59:01 +0800

    improved

--- pagination.go
+++ pagination.go
@@ -1,6 +1,6 @@
 /*
 
-   Copyright 2017 Wenhui Shen <www.webx.top>
+   Copyright since 2017 Wenhui Shen <www.webx.top>
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
@@ -15,6 +15,7 @@
    limitations under the License.
 
 */
+
 package pagination
 
 import (
@@ -251,23 +252,23 @@ func (p *Pagination) List(num ...int) []int {
 	if len(num) > 0 {
 		p.num = num[0]
 	}
-	r := []int{}
-	half := p.num / 2
-	lefts := []int{}
-	for i, j := p.page, p.num; i > 0 && j > half; i-- {
-		lefts = append(lefts, i)
-		j--
-	}
-	c := len(lefts)
-	for i := c - 1; i >= 0; i-- {
-		r = append(r, lefts[i])
-	}
 	pages := p.Pages()
-	for i := p.page + 1; i <= pages && c < p.num; i++ {
-		r = append(r, i)
-		c++
+	var (
+		pList []int
+		count int
+	)
+	start := p.Page() - (p.num / 2)
+	if start < 1 {
+		start = 1
 	}
-	return r
+	for page := start; page <= pages; page++ {
+		count++
+		if count > p.num {
+			break
+		}
+		pList = append(pList, page)
+	}
+	return pList
 }
 
 func (p *Pagination) setDefault() *Pagination {
